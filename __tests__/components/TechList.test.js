e257042535e0997c6b0b0090adbab442706b1649
Maycon Francisco <mayconjf@gmail.com>
2019-10-30 08:09:29 -0300

    added mock and test localStorage

--- __tests__/components/TechList.test.js
+++ __tests__/components/TechList.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import {render, fireEvent} from '@testing-library/react'; //lib que gera um dom do html para realizarmos os testes
+import {render, fireEvent, cleanup} from '@testing-library/react'; //lib que gera um dom do html para realizarmos os testes
 
 import TechList from '~/components/TechList';
 
 describe('Techlist component', () => {
+  // Nao devemos testar APIs externas em nossos testes - usa um mock
+  beforeEach(() => {
+    localStorage.clear();
+  })
+
   it('should be able to add new tech', () => {
     const { getByText, getByTestId, debug, getByLabelText }  = render(<TechList />);
 
@@ -17,5 +22,24 @@ describe('Techlist component', () => {
 
     expect(getByTestId('tech-list')).toContainElement(getByText('Node.js'));
     expect(getByLabelText('Tech')).toHaveValue('');
+  })
+
+  //Teste do localstorage
+  it('should store techs in storage', () => {
+    let { getByTestId, getByLabelText, getByText } = render(<TechList />);
+
+    fireEvent.change(getByLabelText('Tech'), {target: {value: 'Node.js'}});
+    fireEvent.submit(getByTestId('tech-form'));
+
+    expect(localStorage.setItem).toHaveBeenCalledWith('techs', JSON.stringify(['Node.js']))
+
+    cleanup(); //limpa toda a dom - quase que como o F5 para o react renderizar toda a página novamente
+
+    ({ getByTestId, getByLabelText, getByText } = render(<TechList />)); //renderizamos o componente novamente
+
+    expect(localStorage.getItem).toHaveBeenCalledWith('techs');
+    expect(getByTestId('tech-list')).toContainElement(getByText('Node.js'));
+    expect(getByLabelText('Tech')).toHaveValue('');
+
   })
 })
--- readinfolibs.md
+++ readinfolibs.md
@@ -7,13 +7,15 @@ trocar o react-scrips por react-app-rewired nos scrips do package.json (exceto e
 criar o config-overrides.js com pelo menos o module.exports vazio por ser obrigatorio <br/>
 remove a config do eslint no package.json <br/>
 adicionar ao package.json a chave jest com as configurações do jest para testes <br/>
+PS: esta funcionando com a versao "react-scripts": "3.0.1" a 3.2.0 nao funciona <br/>
 "jest": {
   "testMatch": [
     "**/__tests__/**/*.test.js"
   ],
   "setupFilesAfterEnv": [
-    "@testing-library/react/cleanup-after-each",
-    "@testing-library/jest-dom/extend-expect"
+    "@testing-library/react/cleanup-after-each", //add cleanup depois de renderizar, chama cleanup e apaga o dom para poder renderizar novamente
+    "@testing-library/jest-dom/extend-expect", //extende as expectations para testes add toHaveValue e toContainElemente por exemplo
+    "jest-localstorage-mock" //substitui automagicamente o localstorage
   ],
   "moduleNameMapper": {
     "^~/(.*)": "<rootDir>/src/$1"
@@ -25,3 +27,6 @@ a primeira lib eh para ajudar a testar os componentes que criamos  <br/>
 a segundo lib adiciona a possiblidade de testarmos HTML (dom) atraves do JEST, ja que ele so conhece JS
 
 yarn add @types/jest -D // add typescript para ter o intellisense
+
+yarn add jest-localstorage-mock -D //mock para localstorage <br/>
+add "jest-localstorage-mock" no setupFilesAfterEnv no package.json
